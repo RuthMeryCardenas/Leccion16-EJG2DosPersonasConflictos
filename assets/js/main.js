645b603652d4726bc8688452d2e9f822e17c39e1
Nicolas Rasmussen <Nicolas Rasmussen>
2017-03-05 21:52:58 -0500

    Function imc

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,13 @@
 var calculateIMC = document.getElementById('submit');
 calculateIMC.onclick = function (){
-    //Escribe tu código aqui
-    //alert("Hola");
-  }
+  var weight = document.getElementById("weight").value;
+  var height = document.getElementById("height").value;
+
+  var imc = (weight/(height*height)).toFixed(2);
+
+if(weight==="" || height=== ""){
+    alert("Ingresa los datos");
+}else{
+  alert("Tu IMC es: "+imc);
+}
+}
